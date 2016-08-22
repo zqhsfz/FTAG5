468ef181623e6d2fa41bc5a7109f58cbcfd6aa15
Dan Guest <dguest@cern.ch>
2016-08-22 19:54:33 +0200

    more packages checked out

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# checkout packages
 pkgco.py DerivationFrameworkFlavourTag-00-01-55
+cmt co -r MVAUtils-00-00-04 Reconstruction/MVAUtils
+pkgco.py BTagging-00-07-64
+pkgco.py JetTagTools-01-00-96-branch
+pkgco.py AODFix-00-03-26
