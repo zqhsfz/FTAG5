33d50ab44bbac903d943b065aca75f3620c79aef
Dan Guest <dguest@cern.ch>
2016-08-23 10:28:50 +0200

    patch to disable JVC harder

--- scripts/patch_packages.sh
+++ scripts/patch_packages.sh
@@ -5,3 +5,7 @@ set -eu
 JVC_PATCH=$TestArea/scripts/disable-jvc.patch
 AODFIX=$TestArea/Reconstruction/AODFix/python/AODFix_r207.py
 patch $AODFIX < $JVC_PATCH
+
+JVC_PATCH_HARDER=$TestArea/scripts/disable-jvc-harder.patch
+LOAD_TOOLS=$TestArea/PhysicsAnalysis/JetTagging/JetTagAlgs/BTagging/python/BTaggingConfiguration_LoadTools.py
+patch $LOAD_TOOLS < $JVC_PATCH_HARDER
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,9 @@ set -eu
 cd WorkArea/run
 
 Reco_tf.py \
---preExec 'from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-18"' \
---inputAODFile \
-/afs/cern.ch/user/n/nwhallon/work/public/xAOD_samples/mc15_13TeV.301523.MadGraphPythia8EvtGen_A14NNPDF23LO_RS_G_hh_bbbb_c20_M2000.merge.AOD.e3820_s2608_s2183_r7772_r7676/AOD.08176783._000001.pool.root.1 \
---outputDAODFile FTAG5.pool.root \
---reductionConf FTAG5 | tee log_FTAG5.dat
+    --preExec 'from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = "BTagCalibRUN12-08-18"' \
+    --inputAODFile \
+    /afs/cern.ch/user/n/nwhallon/work/public/xAOD_samples/mc15_13TeV.301523.MadGraphPythia8EvtGen_A14NNPDF23LO_RS_G_hh_bbbb_c20_M2000.merge.AOD.e3820_s2608_s2183_r7772_r7676/AOD.08176783._000001.pool.root.1 \
+           --outputDAODFile FTAG5.pool.root \
+           --maxEvents 10 \
+           --reductionConf FTAG5 | tee log_FTAG5.dat
