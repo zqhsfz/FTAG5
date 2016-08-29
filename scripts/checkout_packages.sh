be2301a7afc470133f14bfcebec1c6867c561348
Dan Guest <dguest@cern.ch>
2016-08-29 16:39:49 +0200

    updates to package tags, removed one of two JVC patches

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -2,8 +2,11 @@
 
 set -eu
 
+pkgco.py ParticleJetTools-00-03-33-08
 pkgco.py DerivationFrameworkFlavourTag-00-01-58
 cmt co -r MVAUtils-00-00-04 Reconstruction/MVAUtils
+
 pkgco.py BTagging-00-07-63-branch
 pkgco.py JetTagTools-01-00-96-branch
+
 pkgco.py AODFix-00-03-26
--- scripts/patch_packages.sh
+++ scripts/patch_packages.sh
@@ -5,7 +5,3 @@ set -eu
 JVC_PATCH=$TestArea/scripts/disable-jvc.patch
 AODFIX=$TestArea/Reconstruction/AODFix/python/AODFix_r207.py
 patch $AODFIX < $JVC_PATCH
-
-JVC_PATCH_HARDER=$TestArea/scripts/disable-jvc-harder.patch
-LOAD_TOOLS=$TestArea/PhysicsAnalysis/JetTagging/JetTagAlgs/BTagging/python/BTaggingConfiguration_LoadTools.py
-patch $LOAD_TOOLS < $JVC_PATCH_HARDER
