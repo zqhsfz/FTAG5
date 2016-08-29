4a8bb51929cb4dd964b8f811464cf03e79c1d4ef
Dan Guest <dguest@cern.ch>
2016-08-29 15:41:54 +0200

    bump derivation framework tag, move b-tagging onto a branch

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-pkgco.py DerivationFrameworkFlavourTag-00-01-55
+pkgco.py DerivationFrameworkFlavourTag-00-01-58
 cmt co -r MVAUtils-00-00-04 Reconstruction/MVAUtils
-pkgco.py BTagging-00-07-64
+pkgco.py BTagging-00-07-63-branch
 pkgco.py JetTagTools-01-00-96-branch
 pkgco.py AODFix-00-03-26
