ce99b7c2d8c0e6a202b9d5a34928c2db2a9117ce
Dan Guest <dguest@cern.ch>
2016-09-02 17:59:50 +0200

    Patch to move ExKt

--- scripts/patch_packages.sh
+++ scripts/patch_packages.sh
@@ -5,3 +5,7 @@ set -eu
 JVC_PATCH=$TestArea/scripts/disable-jvc.patch
 AODFIX=$TestArea/Reconstruction/AODFix/python/AODFix_r207.py
 patch $AODFIX < $JVC_PATCH
+
+EXKT_PATCH=$TestArea/scripts/exkt.patch
+DERIVATION_DIR=$TestArea/PhysicsAnalysis/DerivationFramework/DerivationFrameworkFlavourTag
+patch -p0 -N -d $DERIVATION_DIR < $EXKT_PATCH
