832fa6548f87db6376705cde3974231e0c59b286
Dan Guest <dguest@cern.ch>
2016-12-13 01:48:50 +0100

    Use the right checkout script for VR jets

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 . scripts/setup.sh
-./scripts/checkout_packages_ExKtCoM.sh
+./scripts/checkout_packages_VR.sh
 ./scripts/patch_packages.sh
 ./scripts/compile.sh
 echo "use scripts/run.sh to run jobs"
